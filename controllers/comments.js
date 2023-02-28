954cb489a3f57bbb95b4dd22f7318d63e5e7b5ea
Justin Lee <38745924+plumsily@users.noreply.github.com>
2023-02-27 22:28:12 -0800

    added comments

---
+++ controllers/comments.js
@@ -0,0 +1,46 @@
+const Comment = require("../models/Comment");
+const Post = require("../models/Post");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        user: req.user.id,
+        post: req.params.id,
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/" + req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  likeComment: async (req, res) => {
+    try {
+      const comments = await Comment.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $inc: { likes: 1 },
+        },
+        { new: true }
+      );
+      console.log("Comment Likes +1");
+      res.redirect(`/post/${comments.post}`);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  deleteComment: async (req, res) => {
+    try {
+      // Find comment by id
+      let comments = await Comment.findById({ _id: req.params.id });
+      // Delete post from db
+      await Comment.remove({ _id: req.params.id });
+      console.log("Deleted Comment");
+      res.redirect(`/post/${comments.post}`);
+    } catch (err) {
+      res.redirect("/profile");
+    }
+  },
+};
